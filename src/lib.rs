d750d90a9a20a84a54fe3a9f0cab3146a5de0136
Almog Gavra <almog.gavra@gmail.com>
2025-05-15 10:51:48 -0700

    add projection implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,18 @@
 mod error;
+mod ops;
 mod serde;
 mod types;
 mod varint;
-pub mod writer;
+mod writer;
 
 pub use error::ImprintError;
+pub use ops::Project;
 pub use serde::{Read, Write};
 pub use types::{
     DirectoryEntry, Flags, Header, ImprintRecord, MAGIC, SchemaId, TypeCode, VERSION, Value,
 };
 pub use varint::{decode as decode_varint, encode as encode_varint};
+pub use writer::ImprintWriter;
 
 /// Result type for Imprint operations
 pub type Result<T> = std::result::Result<T, error::ImprintError>;
